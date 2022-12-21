8a60e3c69ded9c57151b440b57496ee3fd67e70c
Kutlu Özdemir <kozdemir141@outlook.com>
2022-12-21 15:43:56 +0300

    Origin

---
+++ DefaultExchange/DefaultExchange.publisher/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://pmopgutd:edmooHf15DdFBhXhm4ZtLQ3UCbSp40xQ@chimpanzee.rmq.cloudamqp.com/pmopgutd");
+
+using var connection = factory.CreateConnection();
+
+// RabitMq ya mesaj göndermek için bir kuyruğun olması lazım yoksa mesaj boşa düşer.
+// IModel geriye dönüyor bu benim kanalıma(channel) karşılık geliyor.
+// Bu Channel üzerinden rabbitMq ile iletişime geçebiliriz.
+var channel = connection.CreateModel();
+
+// Kuyruğu oluşturduk.
+// durable = RabitMQ da oluşan kuyruklar fiziksel olarak tutulsun mu?
+// exclusive = Bu kuyruğa sadece buradan oluşturulan kanal üzerinden mi erişilsin?
+// autoDelete = Subscriber down olursa kuyruk silinsin mi?
+channel.QueueDeclare("hello-queue", true, false, false);
+
+// Tek seferde çalıştığında 50 tane mesaj kuyruğa gidicek.
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    string message = $"Message {x}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    // Exchange kullanmadığımızdan dolayı default olarak default exchange kullanmış oluyoruz bu yüzden routing key imize kuyruğumuzdaki ismi vermemiz gerekiyor.
+
+    Console.WriteLine($"Mesaj Gönderilmiştir : {message}");
+});
+
+Console.ReadLine();
---
+++ DefaultExchange/DefaultExchange.subscriber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://pmopgutd:edmooHf15DdFBhXhm4ZtLQ3UCbSp40xQ@chimpanzee.rmq.cloudamqp.com/pmopgutd");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.BasicQos(0,1,false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+// autoAck = İlgili mesajları kuyruktan silmezf
+channel.BasicConsume("hello-queue", false, consumer);
+
+consumer.Received += (sender, eventArgs) =>
+{
+    var message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+
+    Thread.Sleep(1500);
+    
+    Console.WriteLine("Gelen Mesaj:" + message);
+    
+    // İlgili mesajları artık silebilirsin.
+    channel.BasicAck(eventArgs.DeliveryTag,false);
+};
+
+Console.ReadLine();
+
+
