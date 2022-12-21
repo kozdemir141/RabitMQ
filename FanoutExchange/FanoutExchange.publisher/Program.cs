705ea9dc694eece28dfdde3cbfb2d2ba136e4ae0
Kutlu Özdemir <kozdemir141@outlook.com>
2022-12-21 18:16:49 +0300

    origin

--- FanoutExchange/FanoutExchange.publisher/Program.cs
+++ FanoutExchange/FanoutExchange.publisher/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using RabbitMQ.Client;
 
-Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://pmopgutd:edmooHf15DdFBhXhm4ZtLQ3UCbSp40xQ@chimpanzee.rmq.cloudamqp.com/pmopgutd");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+// Exchange Declare ettim, Kuyruk(Queue) Subscriber tarafında declare edilecek.
+channel.ExchangeDeclare("logs-fanout",ExchangeType.Fanout,true);
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    string message = $"log {x}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("logs-fanout", "", null, messageBody);
+    
+    Console.WriteLine($"Mesaj Gönderilmiştir : {message}");
+});
+
+Console.ReadLine();
--- FanoutExchange/FanoutExchange.subscriber/Program.cs
+++ FanoutExchange/FanoutExchange.subscriber/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://pmopgutd:edmooHf15DdFBhXhm4ZtLQ3UCbSp40xQ@chimpanzee.rmq.cloudamqp.com/pmopgutd");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+// Her bir Instance (Consumer'in) kendisine ait kuyruğu olması için random kuyruk isimleri belirledik.
+var randomQueueName = channel.QueueDeclare().QueueName;
+
+// var randomQueueName = "log-database-save-queue";
+// channel.QueueDeclare(randomQueueName, true, false, false);
+
+// Uygulama her ayağa kalktığında ilgili kuyruk oluşacak, uygulama down olduğunda kuyruk silinecek.
+channel.QueueBind(randomQueueName,"logs-fanout","",null);
+
+channel.BasicQos(0,1,false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+Console.WriteLine("Loglar dinleniyor...");
+
+consumer.Received += (sender, eventArgs) =>
+{
+    var message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+
+    Thread.Sleep(1500);
+    
+    Console.WriteLine("Gelen Mesaj:" + message);
+    
+    channel.BasicAck(eventArgs.DeliveryTag,false);
+};
+channel.BasicConsume(randomQueueName, false, consumer);
+
+Console.ReadLine();
