409b6d1c6624ac2d2fa088bba6ef92f29fda93a0
Kutlu Özdemir <kozdemir141@outlook.com>
2022-12-26 12:43:09 +0300

    origin

--- DirectExchange/DirectExchange.subscriber/Program.cs
+++ DirectExchange/DirectExchange.subscriber/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://pmopgutd:edmooHf15DdFBhXhm4ZtLQ3UCbSp40xQ@chimpanzee.rmq.cloudamqp.com/pmopgutd");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+Console.WriteLine("Loglar dinleniyor...");
+
+consumer.Received += (sender, eventArgs) =>
+{
+    var message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+
+    Thread.Sleep(1500);
+
+    Console.WriteLine("Gelen Mesaj:" + message);
+
+    File.AppendAllText("log-critical.txt", message + "\n");
+
+    channel.BasicAck(eventArgs.DeliveryTag, false);
+};
+
+var queueName = "direct-queue-Critical";
+channel.BasicConsume(queueName, false, consumer);
+
+Console.ReadLine();
