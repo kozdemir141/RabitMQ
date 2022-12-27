6c9f89945c5c08a2d4cdec0a8efea6b555b8ee34
Kutlu Özdemir <kozdemir141@outlook.com>
2022-12-27 12:40:50 +0300

    origin

---
+++ TopicExchange/TopicExchange.publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://pmopgutd:edmooHf15DdFBhXhm4ZtLQ3UCbSp40xQ@chimpanzee.rmq.cloudamqp.com/pmopgutd");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("logs-direct", ExchangeType.Direct, true);
+
+Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+{
+    var routeKey = $"route-{x}";
+    var queueName = $"direct-queue-{x}";
+
+    channel.QueueDeclare(queueName, true, false, false);
+    channel.QueueBind(queueName, "logs-direct", routeKey, null);
+});
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    LogNames log = (LogNames)new Random().Next(1, 5);
+
+    string message = $"log-type {log}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    var routeKey = $"route-{log}";
+
+    channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+
+    Console.WriteLine($"Log Gönderilmiştir : {message}");
+});
+
+Console.ReadLine();
+
+public enum LogNames
+{
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
